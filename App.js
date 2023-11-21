39aaa15b46cd641c39b756962d771c667c1f65cf
Eren AVAR <eren.avar@hotmail.com>
2023-11-21 07:17:09 +0300

    Added Search Bar & Expo Vector

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import SearchScreens from "./screens/SearchScreens";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator screenOptions={{ headerTitle: "Restaurant App" }}>
+        <Stack.Screen name="Search" component={SearchScreens} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- screens/SearchScreens.js
+++ screens/SearchScreens.js
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import SearchBar from "../components/SearchBar";
+
 
 export default function SearchScreens() {
   return (
     <View>
-      <Text>SearchScreens</Text>
+      <SearchBar />
     </View>
   );
 }
