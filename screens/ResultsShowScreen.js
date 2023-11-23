c1486944c2bd65b27f3b5234d9cf37fa97580408
Eren AVAR <eren.avar@hotmail.com>
2023-11-23 23:10:46 +0300

    Styles Result Show Screen

--- screens/ResultsShowScreen.js
+++ screens/ResultsShowScreen.js
@@ -1,7 +1,8 @@
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList, StyleSheet } from "react-native";
 import React, { useEffect, useState } from "react";
 import Yelp from "../components/api/Yelp";
 import { Image } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const ResultsShowScreen = ({ route }) => {
   const [result, setResult] = useState(null);
@@ -22,14 +23,24 @@ const ResultsShowScreen = ({ route }) => {
 
   return (
     <View>
-      <Text>{result.name}</Text>
-      <Text>{result.phone}</Text>
+      <Text style={styles.title}>{result.name}</Text>
+      <Text style={styles.phone}>{result.phone}</Text>
+      <View style={styles.icon}>
+        {result.is_closed ? (
+          <MaterialCommunityIcons
+            name="close-box-outline"
+            size={30}
+            color="black"
+          />
+        ) : (
+          <MaterialCommunityIcons name="door-open" size={30} color="black" />
+        )}
+      </View>
+
       <FlatList
         data={result.photos}
         renderItem={({ item }) => {
-          return (
-            <Image style={{ width: 50, height: 50 }} source={{ uri: item }} />
-          );
+          return <Image style={styles.image} source={{ uri: item }} />;
         }}
       />
     </View>
@@ -37,3 +48,23 @@ const ResultsShowScreen = ({ route }) => {
 };
 
 export default ResultsShowScreen;
+
+const styles = StyleSheet.create({
+  image: {
+    height: 180,
+    margin: 10,
+    borderRadius: 20,
+  },
+  phone: {
+    alignSelf: "center",
+    fontSize: 20,
+  },
+  title: {
+    alignSelf: "center",
+    fontSize: 25,
+    marginVertical: 10,
+  },
+  icon: {
+    alignSelf: "center",
+  },
+});
