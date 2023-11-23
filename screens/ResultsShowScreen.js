43775a063e5c4fab006c9dd55ba8225ae27fa608
Eren AVAR <eren.avar@hotmail.com>
2023-11-23 22:38:31 +0300

    Pulled Restaurant Data to Detail Page

--- screens/ResultsShowScreen.js
+++ screens/ResultsShowScreen.js
@@ -1,10 +1,37 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, FlatList } from "react-native";
+import React, { useEffect, useState } from "react";
+import Yelp from "../components/api/Yelp";
+import { Image } from "react-native";
 
-const ResultsShowScreen = () => {
+const ResultsShowScreen = ({ route }) => {
+  const [result, setResult] = useState(null);
+  const id = route.params.id;
+
+  const getResult = async (id) => {
+    const response = await Yelp.get(`/${id}`);
+    setResult(response.data);
+  };
+
+  useEffect(() => {
+    getResult(id);
+  }, []);
+
+  if (!result) {
+    return null;
+  }
+
   return (
     <View>
-      <Text>ResultsShowScreen</Text>
+      <Text>{result.name}</Text>
+      <Text>{result.phone}</Text>
+      <FlatList
+        data={result.photos}
+        renderItem={({ item }) => {
+          return (
+            <Image style={{ width: 50, height: 50 }} source={{ uri: item }} />
+          );
+        }}
+      />
     </View>
   );
 };
